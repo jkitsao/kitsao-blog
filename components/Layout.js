486c69d34a794703997a733ad89ae7e621aa0869
Jackson Kitsao <kitsaojackson22@gmail.com>
2020-06-30 11:24:06 +0300

    added banner to root page

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,4 @@
-import Link from "next/link";
+// import Link from "next/link";
 import { useRouter } from "next/router";
 import Badges from "../components/Badges";
 import Outerlinks from "./Outerlinks";
@@ -8,22 +8,27 @@ export default function Layout({ children }, props) {
 
   const header = isRoot ? (
     <span className=" sm:px-8 py-2 pt-2">
-      <p className="text-xl leading-normal sm:text-2xl text-left text-purple-900 sm:leading-none font-normal text-center">
-        Fun, practical and engaging web development blogs and stories
-      </p>
       <Badges />
     </span>
   ) : (
-    <h1 className="mb-2">
-      <span className="text-xl font-hairline no-underline">
+    <h1 className="mb-2 py-3 lg:py-2">
+      <span className="text-xl font-hairline">
         <Outerlinks />
       </span>
     </h1>
   );
-  console.log(props);
+  // console.log(props);
+  const banner = isRoot ? (
+    <div className="py-4">
+      <h2 className="text-xl leading-relaxed sm:text-4xl text-left sm:leading-none text-center text-indigo-900">
+        Fun, practical and engaging web development blogs and stories
+      </h2>
+    </div>
+  ) : null;
   return (
     <div className="max-w-screen-lg px-4  mx-auto">
-      <header className="mb-0 bg-gray-300 px-3 shadow-lg mt-3 lg:py-5">
+      {banner}
+      <header className="mb-0 bg-grey-900 px-3 shadow-lg lg:py-4">
         {header}
       </header>
       <main className="my-2 ">{children}</main>
