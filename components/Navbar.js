add138050b28ce7bb8c3d870a81212bc9f02da25
Jackson Kitsao <kitsaojackson22@gmail.com>
2020-07-03 01:19:16 +0300

    add logo to header

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,17 @@
 import React from "react";
 import Link from "next/link";
+import Logo from "./Logo";
 export default function Navbar() {
   return (
     <nav className=" bg-black flex sticky top-0 z-10 justify-center">
       <span className="cursor-pointer">
         <Link href="/">
-          <h2 className="text-orange-500 text-xl sm:text-3xl">Devcrunch.io</h2>
+          <div>
+            <Logo className="" />
+            <h2 className="text-orange-500 text-lg sm:text-4xl inline-block">
+              Devcrunch.io
+            </h2>
+          </div>
         </Link>
       </span>
     </nav>
