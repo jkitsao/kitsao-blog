19e504f0e98f1d7173917850c0ba0c3b6b99b8d7
Jackson Kitsao <kitsaojackson22@gmail.com>
2020-07-13 19:57:36 +0300

    adjust newsletter modal size

--- components/newsletter/Newsletter.js
+++ components/newsletter/Newsletter.js
@@ -42,13 +42,13 @@ const Letterform = ({
   };
   return (
     <div
-      className=" bg-gray-900 lg:pt-12 sm:pb-32  lg:px-48 z-50 shadow-2xl  fixed"
+      className=" bg-gray-900 lg:pt-12 py-12 sm:pb-32  lg:px-48 z-50 shadow-2xl  fixed"
       style={{
         background:
           "linear-gradient(90deg, rgba(36,2,43,1) 0%, rgba(24,1,16,1) 50%, rgba(46,8,3,1) 100%)",
       }}
     >
-      <div className=" ml-10 sm:ml-16">
+      <div className=" px-4 sm:ml-16">
         <span
           onClick={handleClick}
           className="text-white cursor-pointer bg-red-700 sm:py-1 px-1 active:bg-green-800 sm:font-bold absolute right-0 top-0 "
@@ -56,13 +56,13 @@ const Letterform = ({
           cancel
         </span>
         <h3 className="text-lg text-white font-bold mt-12 text-xl">
-          Sign up to our news letter
+          subscribe to my NewsLetter
         </h3>
 
         <div className="justify-center items-center flex mt-3">
           <form className="flex flex-wrap flex-row">
             <input
-              className="sm:p-2 text-sm  outline-none  my-2 mx-2 w-3/4"
+              className="sm:p-2 text-sm  outline-none  my-2 mx-2 w-full sm:w-3/4"
               id="text"
               type="text"
               aria-label="your name"
@@ -72,7 +72,7 @@ const Letterform = ({
               required
             />
             <input
-              className="sm:p-2 text-sm  outline-none mx-1 my-2 mx-2 w-3/4"
+              className="sm:p-2 text-sm  outline-none mx-1 my-2 mx-2 w-full sm:w-3/4"
               id="email"
               type="email"
               aria-label="email address"
@@ -82,11 +82,11 @@ const Letterform = ({
               required
             />
             <button
-              className="bg-blue-600 hover:bg-blue-900 my-2 w-3/4 mx-2 duration-300 text-white border-none px-5 py-2 sm:py-3 outline-none cursor-pointer"
+              className="bg-blue-600 hover:bg-blue-900 my-2 py-1 text-sm w-full sm:w-3/4 mx-2 duration-300 text-white border-none px-5  sm:py-3 outline-none cursor-pointer"
               type="submit"
               onClick={handleSubmit}
             >
-              Sign Up
+              Subscribe
             </button>
           </form>
         </div>
