4d995be486314f5cbb49248087bd366cebc6b394
Jackson Kitsao <kitsaojackson22@gmail.com>
2020-12-11 16:30:00 +0300

    add dynamic sitemap data

--- next.config.js
+++ next.config.js
@@ -2,3 +2,12 @@ const withPlugins = require("next-compose-plugins");
 const optimizedImages = require("next-optimized-images");
 
 module.exports = withPlugins([optimizedImages]);
+module.exports = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      require("./scripts/generate-sitemap");
+    }
+
+    return config;
+  },
+};
